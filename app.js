cfbaa0db3dcaabcfbbacdb5df590096f551cb565
Zfinix <ogbondachiziaruhoma@gmail.com>
2018-09-06 11:04:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var sRouter = require('./routers/router');
-
+var cors = require('cors') 
 var app = express();
+app.use(cors());
 var PORT = 8081;
 var HOST_NAME = 'localhost:27017';
 var DATABASE_NAME = 'students';
@@ -15,6 +16,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
+
 
 app.use('/api', sRouter);
 
